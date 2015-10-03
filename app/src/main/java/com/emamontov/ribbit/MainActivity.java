349ce754bf5ea78a61760b36c9135d45e420a7af
Malemute <erlinn@fromru.com>
2015-10-03 18:14:48 +0300

    List of Messages Created

--- app/src/main/java/com/emamontov/ribbit/MainActivity.java
+++ app/src/main/java/com/emamontov/ribbit/MainActivity.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import android.app.AlertDialog;
